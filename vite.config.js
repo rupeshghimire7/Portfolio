9f19464844fdd0c7a44685fc8ffdebeb196a042e
Rupesh Ghimire <rupeshghimire007@gmail.com>
2025-08-18 15:44:10 +0545

    Static paths

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // For a GitHub user site (username.github.io) base should be '/'
   // For a project site (username.github.io/repo) change this to `'/REPO_NAME/'`
-  base: '/'
+  base: '/Portfolio/'
 })
