d4e53922fa85d8e816c847795bc5891a7fcc0104
Rupesh Ghimire <rupeshghimire007@gmail.com>
2025-08-18 14:36:59 +0545

    Browser router changes

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import './styles/tailwind.css'
@@ -19,7 +20,7 @@ import './styles/tailwind.css'
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='/Portfolio'>
         <App />
       </BrowserRouter>
     </HelmetProvider>
